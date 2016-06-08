44a63cf440ee69d429831525e09be5f6e3e2d9d8
Nicolas Mesa <nicolasmesa@Nicolass-MacBook-Air.local>
2016-06-08 14:52:30 -0500

    Task 1 complete

--- main.js
+++ main.js
@@ -1,2 +1,19 @@
 // GO!
+var divState = {
+        showing: true
+    }
 
+document.querySelector("#hide-nav button").addEventListener('click',function(){
+    var navNode = document.querySelector('.nav-menu')
+    if (divState.showing) {
+        navNode.style.opacity = 0
+        divState.showing = false
+        document.querySelector('button').textContent = "show Now"
+    }
+    else {
+        navNode.style.opacity = 1
+        divState.showing = true
+        document.querySelector('button').textContent = "hide now"
+
+    } console.log(divState)
+})
